036a893b89c7845239653f1007d0b237fe61d10f
Vsevolod <11112365@mail.ru>
2019-11-21 11:29:53 +0300

    Initial Commit

---
+++ Find a bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find a bar
+//
+//  Created by Всеволод Андрющенко on 21.11.2019.
+//  Copyright © 2019 Всеволод Андрющенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
